b31da2dae9564ae01dcd54dd1c38b35a1dda432e
Andres Condo <1dkevinandres.b@gmail.com>
2020-04-24 11:37:23 -0500

    añadido hexagono

---
+++ figura.py
@@ -0,0 +1,23 @@
+import turtle
+
+window = turtle.Screen()
+andres = turtle.Turtle()
+andres.forward(100)
+andres.left(45)
+andres.forward(100)
+andres.left(45)
+andres.forward(100)
+andres.left(45)
+andres.forward(100)
+andres.left(45)
+andres.forward(100)
+andres.left(45)
+andres.forward(100)
+andres.left(45)
+andres.forward(100)
+andres.left(45)
+andres.forward(100)
+andres.left(45)
+andres.forward(100)
+
+turtle.mainloop()
